992ade9faf76e691cd5f2db6f4dcfce9cb634f18
zaazxz <mirzaqamaruzzaman18@gmail.com>
2023-11-21 20:32:38 +0700

    feat(SKYE-1-form-slicing) : form registration slicing 1

---
+++ resources/views/components/timeline/timeline.blade.php
@@ -0,0 +1,48 @@
+<div class="container ">
+    <div class="row text-center justify-content-center mb-5">
+        <div class="col-xl-6 col-lg-8">
+            <h2 class="font-weight-bold">Registration</h2>
+            <p class="text-muted timeline-section">
+                Step Timeline
+            </p>
+        </div>
+    </div>
+
+    <div class="row timeline-section">
+        <div class="col">
+            <div class="timeline-steps aos-init aos-animate" data-aos="fade-up">
+                <div class="timeline-step">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2003">
+                        <i class="fa-solid fa-user icon-circle icon-active" ></i>
+                        <p class="h6 text-muted mb-0 mb-lg-0 mt-3">Identitas Siswa</p>
+                    </div>
+                </div>
+                <div class="timeline-step">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2004">
+                        <i class="fa-solid fa-graduation-cap icon-circle"></i>
+                        <p class="h6 text-muted mb-0 mb-lg-0 mt-3">Asal Sekolah</p>
+                    </div>
+                </div>
+                <div class="timeline-step">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2005">
+                        <i class="fa-solid fa-user-group icon-circle" ></i>
+                        <p class="h6 text-muted mb-0 mb-lg-0 mt-3">Orang Tua</p>
+                    </div>
+                </div>
+                <div class="timeline-step">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2010">
+                        <i class="fa-solid fa-file-lines icon-circle" ></i>
+                        <p class="h6 text-muted mb-0 mb-lg-0 mt-3">Data Periodik</p>
+                    </div>
+                </div>
+                <div class="timeline-step mb-0">
+                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2020">
+                        <i class="fa-solid fa-check icon-circle" ></i>
+                        <p class="h6 text-muted mb-0 mb-lg-0 mt-3">Selesai</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
