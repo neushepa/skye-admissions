1ab61be640bb98090faa3aa343ff6164d9f18575
neushepa <r.andrianto@gmail.com>
2023-11-21 16:24:35 +0700

    initial commit

---
+++ resources/views/header/hero/hero-a.blade.php
@@ -0,0 +1,20 @@
+<section id="hero" class="hero d-flex align-items-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-4">
+                <h2 data-aos="fade-up">Pendaftaran Student</h2>
+                <blockquote data-aos="fade-up" data-aos-delay="100">
+                    <p>Masa Depan adalah Hari Ini, Jangan Biarkan Putra-Putri Kita Salah Dalam Melangkah Menuju Masa
+                        Depan</p>
+                </blockquote>
+                <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
+                    <a href="#about" class="btn-get-started">Register Now</a>
+                    <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+                        class="glightbox btn-watch-video d-flex align-items-center"><i
+                            class="bi bi-play-circle"></i><span>Watch Video</span></a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Skye Digipreneur</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="../front/img/favicon.png" rel="icon">
+    <link href="../front/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="../front/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../front/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="../front/vendor/aos/aos.css" rel="stylesheet">
+    <link href="../front/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="../front/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="../front/vendor/remixicon/remixicon.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="../front/css/main.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Nova
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/nova-bootstrap-business-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body class="page-index">
+
+    @yield('content')
+
+    <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <div id="preloader"></div>
+
+    <!-- Vendor JS Files -->
+    <script src="../front/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../front/vendor/aos/aos.js"></script>
+    <script src="../front/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="../front/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="../front/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="../front/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="../front/js/main.js"></script>
+
+</body>
+
+</html>
